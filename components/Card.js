f4abb008829361ba097afeb63ebcf8d5770204d8
Allison McHugh <Allison.McHugh@hgem.com>
2023-03-25 18:25:02 +0000

    first commit

---
+++ components/Card.js
@@ -0,0 +1,12 @@
+import React from "React";
+
+export default function Card(){
+    return (
+        <div>
+            <img></img>
+            <div>
+                <span>Enter text here</span>
+            </div>
+        </div>
+    )
+}
