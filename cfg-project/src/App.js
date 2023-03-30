d0729a5d3a2723baf7aadd90a701863080460adf
Allison McHugh <Allison.McHugh@hgem.com>
2023-03-30 18:15:31 +0100

    Anna updates

--- cfg-project/src/App.js
+++ cfg-project/src/App.js
@@ -1,22 +1,34 @@
-import logo from './mountain.jpg';
+import logo from './logo.svg';
 import './App.css';
+import Card from './Card.js';
+import './card.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div>
+              <Card
+              title = 'Yoga Class'
+              imageUrl = 'https://images.unsplash.com/photo-1545389336-cf090694435e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
+              body = 'Card body, lorem ipsum, bla bla bla'
+              />
+          </div>
+          <div>
+              <Card
+              title = 'Indoor cycling'
+              imageUrl = 'https://media.istockphoto.com/id/918931148/photo/fit-people-working-out-at-exercising-class-in-the-gym.jpg?s=612x612&w=0&k=20&c=-2fPHUiGAjZJXeSqZTw8ipfeUOkI_w8H0wEGcbD7oPc='
+              body = 'blablabla'
+              />
+          </div>
+          <div>
+            <Card
+            title = 'Do nothing class'
+            imageUrl = 'https://images.pexels.com/photos/258330/pexels-photo-258330.jpeg?cs=srgb&dl=pexels-pixabay-258330.jpg&fm=jpg'
+            body = 'Class is dedicated to all - parents, workers, aliens. Come, relax and do nothing!'
+            />
+          </div>
       </header>
     </div>
   );
