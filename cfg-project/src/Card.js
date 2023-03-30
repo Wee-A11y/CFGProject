cd9db3954000864d92e87928de96fa72472c0e45
Anna Wasilewska <anna.wasilewska89@gmail.com>
2023-03-30 10:23:37 +0100

    Card component

--- cfg-project/src/Card.js
+++ cfg-project/src/Card.js
@@ -1,14 +1,28 @@
-import React from "React";
+import React from "react";
 
-//Anna if you write your code in here
-
-export default function Card(){
-    return (
-        <div>
-            <img></img>
-            <div>
-                <span>Enter text here</span>
+function Card(props){
+  return (
+    <div className='card-container'>
+      <div className="image-container">
+          <img src={props.imageUrl} alt='' />
+      </div>
+      <div className="card-content">
+            <div className="card-title">
+                <h3>{props.title}</h3>
             </div>
-        </div>
-    )
+            <div className="card-body">
+              <p>{props.body}</p>
+            </div>
+      </div> 
+      <div className="card-button">
+        <button>
+            <a>
+              Click me
+            </a>
+        </button>
+      </div>    
+    </div>
+  )
 }
+
+export default Card
